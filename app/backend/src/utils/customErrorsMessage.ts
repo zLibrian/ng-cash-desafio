9e78f6b709a391d3357ceae1ddee7f16689986ee
Kevin Willian <kevinwillian084@gmail.com>
2022-11-16 20:00:29 -0300

    feat: adiciona novas mensagens customizadas de erro

--- app/backend/src/utils/customErrorsMessage.ts
+++ app/backend/src/utils/customErrorsMessage.ts
@@ -4,6 +4,11 @@ export interface IError {
 
 const errorMap: IError = {
   usernameAlreadyInUse: { message: 'Username already in use', code: 409 },
+  permissionDenied: { message: 'Permission denied', code: 403 },
+  invalidCredentials: { message: 'Incorrect email or password', code: 401 },
+  tokenNotFound: { message: 'Token was not found', code: 401 },
+  'invalid token': { message: 'Token must be a valid token', code: 401 },
+  userNotFound: { message: 'User not found', code: 404 },
 };
 
 export default errorMap;
