7dd1b2747ec2ac552227a641369776012e0b3b60
Kevin Willian <kevinwillian084@gmail.com>
2022-11-16 19:58:40 -0300

    feat: adiciona nova rota de logio ao roteador de usuarios

--- app/backend/src/router/userRouter.ts
+++ app/backend/src/router/userRouter.ts
@@ -4,5 +4,6 @@ import userController from '../controllers/userController';
 const userRouter = express.Router();
 
 userRouter.post('/register', userController.register);
+userRouter.post('/login', userController.login);
 
 export default userRouter;
