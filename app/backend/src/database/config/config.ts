444b1145f04a3739742d9360f82def4900cc107b
Kevin Willian <kevinwillian084@gmail.com>
2022-11-15 23:03:24 -0300

    feat: adiciona configuracoes iniciais do sequelize

---
+++ app/backend/src/database/config/config.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { Options } from 'sequelize';
+
+const config: Options = {
+  host: process.env.DB_HOST || 'db',
+  port: Number(process.env.DB_PORT || 5432),
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || 'password123',
+  database: process.env.DB_NAME || 'ng_cash',
+  dialect: 'postgres',
+  dialectOptions: {
+    timezone: 'Z',
+  },
+  logging: false,
+};
+
+module.exports = config;
