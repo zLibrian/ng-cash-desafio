635041fa700f2ec1a87413204745e9db7076f039
Kevin Willian <kevinwillian084@gmail.com>
2022-11-18 00:47:14 -0300

    feat: adiciona rotas de criaca  e listagem de transacoes

--- app/backend/src/controllers/userController.ts
+++ app/backend/src/controllers/userController.ts
@@ -1,14 +1,21 @@
 import { Request, Response } from 'express';
-import { getUserBalanceSchema, UserRegister, UserRegisterSchema } from '../interfaces/IUser';
 import userService from '../services/userService';
+import HandleToken from '../utils/handleToken';
+
+import { getUserBalanceSchema, UserRegister, UserRegisterSchema } from '../interfaces/IUser';
+import {
+  getTransactionSchema, IGetTransactionBody, IMakeTransactionBody, transactionBodySchema,
+} from '../interfaces/ITransaction';
 
 const userController = {
   register: async (req: Request, res: Response): Promise<Response> => {
     const { username, password }: UserRegister = req.body;
     await UserRegisterSchema.parseAsync({ username, password });
     const newUser = await userService.register({ username, password });
-    return res.status(201).json(newUser);
+    const token = HandleToken.encode(newUser);
+    return res.status(201).json({ token });
   },
+
   login: async (req: Request, res: Response): Promise<Response> => {
     const { username, password }: UserRegister = req.body;
     await UserRegisterSchema.parseAsync({ username, password });
@@ -17,10 +24,33 @@ const userController = {
   },
 
   getUserBalance: async (req: Request, res: Response): Promise<Response> => {
-    const { accountId, username } = req.body;
+    const { accountId, username } = req.user;
     await getUserBalanceSchema.parseAsync({ accountId, username });
     const balance = await userService.getUserBalance(username, accountId);
     return res.status(200).json({ balance });
+  },
+
+  getUserByAccountId: async (req: Request, res: Response): Promise<Response> => {
+    const { accountId } = req.user;
+    const user = await userService.getUserByAccountId(accountId);
+    return res.status(200).json({ user });
+  },
+
+  makeTransaction: async (req: Request, res: Response): Promise<Response> => {
+    const { accountId, username } = req.user;
+    const { value, targetUsername }: IMakeTransactionBody = req.body;
+    await transactionBodySchema.parseAsync({ username, accountId, value, targetUsername });
+    const transaction = await userService
+      .makeTransaction({ username, targetUsername, value, accountId });
+    return res.status(200).json(transaction);
+  },
+
+  getTransactions: async (req: Request, res: Response): Promise<Response> => {
+    const { accountId, username } = req.user;
+    const { type, date }: IGetTransactionBody = req.body;
+    await getTransactionSchema.parseAsync({ type, date });
+    const transactions = await userService.getTransactions({ username, accountId, type, date });
+    return res.status(200).json(transactions);
   },
 };
 
