36f055d027f3ef172dfb39ccd8b622175ebcd513
Kevin Willian <kevinwillian084@gmail.com>
2022-11-18 13:34:50 -0300

    feat: adiciona rotas da aplicacao

--- app/backend/src/router/userRouter.ts
+++ app/backend/src/router/userRouter.ts
@@ -1,11 +1,22 @@
 import express from 'express';
 import userController from '../controllers/userController';
+import { ReqUser } from '../interfaces/IUser';
 import HandleAuth from '../middleware/HandleAuth';
 
 const userRouter = express.Router();
 
+declare module 'express-serve-static-core' {
+  interface Request {
+    user: ReqUser
+  }
+}
+
 userRouter.post('/register', userController.register);
 userRouter.post('/login', userController.login);
-userRouter.get('/user/balance', HandleAuth.handleAuthorization, userController.getUserBalance);
+
+userRouter.get('/user/balance', HandleAuth, userController.getUserBalance);
+userRouter.get('/user/transactions', HandleAuth, userController.getTransactions);
+
+userRouter.post('/user/transaction', HandleAuth, userController.makeTransaction);
 
 export default userRouter;
