932cd8b1a68b15b145beca49231539e6a2720af5
Kevin Willian <kevinwillian084@gmail.com>
2022-11-18 15:33:40 -0300

    build: adiciona tailwind e eslint ao app_frontend

---
+++ app/frontend/src/pages/index.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  const testeTailwind = 'Testando tailwind123!';
+  return (
+    <div>
+      <h1 className="bg-red-700 text-4xl">{testeTailwind}</h1>
+    </div>
+  );
+};
+
+export default Home;
