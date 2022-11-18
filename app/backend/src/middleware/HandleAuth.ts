66917819fba1463f20c9d540ab52a8a197256495
Kevin Willian <kevinwillian084@gmail.com>
2022-11-18 00:40:50 -0300

    feat: adiciona objeto req.user ao parametro Request dos endPoint

--- app/backend/src/middleware/HandleAuth.ts
+++ app/backend/src/middleware/HandleAuth.ts
@@ -2,14 +2,13 @@ import { NextFunction, Request, Response } from 'express';
 import IJwtPayload from '../interfaces/IJwtPayload';
 import HandleToken from '../utils/handleToken';
 
-const HandleAuth = {
-  handleAuthorization: async (req: Request, _res: Response, next: NextFunction) => {
-    const { authorization } = req.headers;
-    if (!authorization) throw new Error('tokenNotFound');
-    const { payload }: IJwtPayload = HandleToken.decode(authorization);
-    if (!payload) throw new Error('invalidToken');
-    next();
-  },
+const HandleAuth = async (req: Request, _res: Response, next: NextFunction) => {
+  const { authorization } = req.headers;
+  if (!authorization) throw new Error('tokenNotFound');
+  const { payload }: IJwtPayload = HandleToken.decode(authorization);
+  if (!payload) throw new Error('invalidToken');
+  req.user = payload;
+  next();
 };
 
 export default HandleAuth;
--- app/backend/src/services/userService.ts
+++ app/backend/src/services/userService.ts
@@ -2,9 +2,11 @@ import { Sequelize } from 'sequelize';
 import * as config from '../database/config/config';
 import Account from '../database/models/Account';
 import User from '../database/models/User';
+import { IGetTransaction, ITransactionBody } from '../interfaces/ITransaction';
 import { User as IUser, UserCreateResponse, UserRegister } from '../interfaces/IUser';
 import handleEncrypt from '../utils/handleEncrypt';
 import HandleToken from '../utils/handleToken';
+import accountService from './Account';
 
 const sequelize = new Sequelize(config);
 
@@ -14,8 +16,9 @@ const userService = {
     if (user) throw new Error('usernameAlreadyInUse');
   },
 
-  userExists: async (username: string): Promise<User | null> => {
+  userExists: async (username: string): Promise<User> => {
     const user = await User.findOne({ where: { username } });
+    if (!user) throw new Error('userNotFound');
     return user;
   },
 
@@ -31,7 +34,7 @@ const userService = {
   register: async ({ username, password }: UserRegister): Promise<UserCreateResponse> => {
     await userService.usernameExists(username);
     const newUser = await sequelize.transaction(async (t): Promise<UserCreateResponse> => {
-      const { id: accountId } = await Account.create({ balance: 700.09 }, { transaction: t });
+      const { id: accountId } = await Account.create({}, { transaction: t });
       const hashedPassword = await handleEncrypt.encrypt(password);
       const user = await User.create(
         { username, password: hashedPassword, accountId },
@@ -50,13 +53,43 @@ const userService = {
     return token;
   },
 
-  getUserBalance: async (username: string, accountId: number): Promise<number> => {
+  isUserAllowed: async (username: string, accountId: number): Promise<void> => {
     const user = await userService.userExists(username);
-    if (!user || user.accountId !== accountId) throw new Error('permissionDenied');
-    const account = await Account.findOne({ where: { id: accountId } });
-    if (!account) throw new Error('userNotFound');
+    if (user.accountId !== accountId) throw new Error('permissionDenied');
+  },
+
+  getUserBalance: async (username: string, accountId: number): Promise<number> => {
+    await userService.isUserAllowed(username, accountId);
+    const account = await accountService.getBalance(accountId);
     const { balance } = account;
-    return balance;
+    return +balance;
+  },
+
+  getUserByAccountId: async (accountId: number): Promise<User> => {
+    const user = await User.findOne({ where: { accountId } });
+    if (!user) throw new Error('userNotFound');
+    return user;
+  },
+
+  makeTransaction: async ({ username, targetUsername, value, accountId }: ITransactionBody) => {
+    if (username === targetUsername) throw new Error('invalidTarget');
+    const [user, { accountId: targetId }] = await Promise.all([
+      userService.userExists(username),
+      userService.userExists(targetUsername),
+    ]);
+    if (user.accountId !== accountId) throw new Error('permissionDenied');
+    const transaction = await accountService.makeTransaction({ accountId, targetId, value });
+
+    return transaction;
+  },
+
+  getTransactions: async ({ username, accountId, type, date }: IGetTransaction) => {
+    await userService.isUserAllowed(username, accountId);
+    const transactions = await accountService.getTransactionByType(accountId, type);
+    const filteredTransactions = date ? transactions.filter((transaction) =>
+      transaction.createdAt.toLocaleDateString() === new Date(date).toLocaleDateString())
+      : transactions;
+    return filteredTransactions;
   },
 };
 
