3cbbec513146ab6e887d7390af3361d87b414a9e
Kevin Willian <kevinwillian084@gmail.com>
2022-11-18 00:22:53 -0300

    feat: adiciona funcao de criar uma transacao no banco de dados

--- app/backend/src/services/Account.ts
+++ app/backend/src/services/Account.ts
@@ -3,7 +3,11 @@ import * as config from '../database/config/config';
 import Account from '../database/models/Account';
 import TransactionModel from '../database/models/Transaction';
 
+import { IAccountCreateTransaction, IAccountTransaction } from '../interfaces/IAccount';
+import { AccTransaction, ITransactionType } from '../interfaces/ITransaction';
+
 const sequelize = new Sequelize(config);
+
 const accountService = {
   getBalance: async (accountId: number) => {
     const account = await Account.findByPk(accountId);
@@ -11,6 +15,36 @@ const accountService = {
     return account;
   },
 
+  createTransaction: async (
+    { creditedAccountId, debitedAccountId, value, t }: IAccountCreateTransaction,
+  ): Promise<TransactionModel> => TransactionModel.create(
+    {
+      creditedAccountId,
+      debitedAccountId,
+      value,
+    },
+    { transaction: t },
+  ),
+
+  makeTransaction: async ({ accountId, targetId, value }: IAccountTransaction): AccTransaction =>
+    sequelize.transaction(async (t) => {
+      const [userAccount, targetUserAccount] = await Promise.all([
+        accountService.getBalance(accountId),
+        accountService.getBalance(targetId),
+      ]);
+      await Promise.all([
+        userAccount.decrement('balance', { by: value, transaction: t }),
+        targetUserAccount.increment('balance', { by: value, transaction: t }),
+      ]);
+      const newTransaction = await accountService.createTransaction({
+        creditedAccountId: targetId,
+        debitedAccountId: accountId,
+        value,
+        t,
+      });
+      const { id: _, ...transactionWithoutId } = newTransaction.toJSON();
+      return transactionWithoutId;
+    }),
 };
 
 export default accountService;
